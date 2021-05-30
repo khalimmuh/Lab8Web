8602d292cf1d78b8d403e0f760f04315a315ed1c
khalimmuh <81312138+khalimmuh@users.noreply.github.com>
2021-05-30 23:46:34 +0700

    menambahkan index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+include("koneksi.php");
+
+// query untuk menampilkan data
+$sql = 'SELECT * FROM data_barang';
+$result = mysqli_query($conn, $sql);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: ;
+}
+</style>
+
+    <meta charset="UTF-8">
+    <title>Data Barang</title>
+</head>
+<body>
+        <div class="container">
+
+            <h1>Data Barang</h1>
+           
+             <h3><a href="tambah.php">Tambah Barang</a></h3>
+
+            <div class="main">
+                <table>
+                <tr>
+                    <th>Gambar</th>
+                    <th>Nama Barang</th>
+                    <th>Katagori</th>
+                    <th>Harga Jual</th>
+                    <th>Harga Beli</th>
+                    <th>Stok</th>
+                    <th>Aksi</th>
+                </tr>
+                <?php if($result): ?>
+                <?php while($row = mysqli_fetch_array($result)): ?>
+                <tr>
+                    <td><img src="gambar/<?= $row['gambar'];?>" alt="<?=
+$row['nama'];?>"></td>
+                    <td><?= $row['nama'];?></td>
+                    <td><?= $row['kategori'];?></td>
+                    <td><?= $row['harga_beli'];?></td>
+                    <td><?= $row['harga_jual'];?></td>
+                    <td><?= $row['stok'];?></td>
+                    <td><a href="ubah.php">Ubah</a> <a href="hapus.php">Hapus</a> </td>
+                </tr>
+                 <?php endwhile; else: ?>
+                <tr>
+                    <td colspan="7">Belum ada data</td>
+                </tr>
+                <?php endif; ?>
+                </table>
+            </div>
+        </div>
+</body>
+</html>
